92b358ea44e412e1ab6ee728b805327d1c139489
Alex Xavier <alexsantosxavier@gmail.com>
2023-05-21 13:48:57 -0300

    Derivating BasePawn to Unreal Blueprint

--- Toon Tanks/Source/ToonTanks/BasePawn.cpp
+++ Toon Tanks/Source/ToonTanks/BasePawn.cpp
@@ -9,9 +9,16 @@ ABasePawn::ABasePawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
-	RootComponent = CapsuleComp;
 
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Collider"));
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn Point"));
+	
+	RootComponent = CapsuleComp;	
+	BaseMesh->SetupAttachment(CapsuleComp);
+	TurretMesh->SetupAttachment(BaseMesh);
+	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
 }
 
 // Called when the game starts or when spawned
