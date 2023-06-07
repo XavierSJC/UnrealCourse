fdcfef60f74b9bce531fae308cece78d481d7b36
Alex Xavier <alexsantosxavier@gmail.com>
2023-06-07 20:31:27 -0300

    Created custom player controller

--- Toon Tanks/Source/ToonTanks/ToonTanksGameMode.cpp
+++ Toon Tanks/Source/ToonTanks/ToonTanksGameMode.cpp
@@ -4,6 +4,7 @@
 #include "ToonTanksGameMode.h"
 #include "Tank.h"
 #include "Tower.h"
+#include "ToonTanksPlayerController.h"
 #include "Kismet/GameplayStatics.h"
 
 void AToonTanksGameMode::ActorDied(AActor* DeadActor)
@@ -11,10 +12,9 @@ void AToonTanksGameMode::ActorDied(AActor* DeadActor)
     if (DeadActor == Tank)
     {
         Tank->HandleDestruction();
-        if (Tank->GetTankPlayerController())
+        if (ToonTanksPlayerController)
         {
-            Tank->DisableInput(Tank->GetTankPlayerController());
-            Tank->GetTankPlayerController()->bShowMouseCursor = false;
+            ToonTanksPlayerController->SetPlayerEnabledState(false);
         }
 
     }
@@ -29,4 +29,5 @@ void AToonTanksGameMode::BeginPlay()
     Super::BeginPlay();
 
     Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+    ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
 }
